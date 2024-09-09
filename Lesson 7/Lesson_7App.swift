2fa516ef434fca39eaf1ba2fdc16dd9a8c27b1b7
Javier <javier_wee_jia_wei@s2024.ssts.edu.sg>
2024-09-09 16:32:24 +0800

    Initial Commit

---
+++ Lesson 7/Lesson_7App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_7App.swift
+//  Lesson 7
+//
+//  Created by Javier Wee on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
